74b964c8a5853bd70d644a4a48584b8a33bdebcc
epireve <i@firdaus.my>
2025-03-14 11:32:18 +0800

    Fix deployment issues with dynamic imports

--- clean-project/src/components/ClientCompanySearch.tsx
+++ clean-project/src/components/ClientCompanySearch.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-
-// Dynamically import the CompanySearch component with ssr disabled
-// This is allowed in client components but not in server components
-const CompanySearch = dynamic(
-  () => import('@/components/CompanySearch').then(mod => ({ default: mod.CompanySearch })),
-  { ssr: false }
-);
+import { CompanySearch } from '@/components/CompanySearch';
 
 export default function ClientCompanySearch() {
   return <CompanySearch />;
